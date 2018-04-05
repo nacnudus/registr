21c9bd0b8beaf5bb64b6020e57d31d15cc6ddce1
Duncan Garmonsway <nacnudus@gmail.com>
2018-04-05 15:30:48 +0100

    Set some more globalVariables

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,21 @@ globalVariables(c("data",
                   "download.file",
                   "unzip",
                   "item_hash",
-                  "item-hash"))
+                  "item-hash",
+                  "hash",
+                  "hash-list",
+                  "as_tibble",
+                  "json",
+                  "entry-number",
+                  "desc",
+                  "map_chr",
+                  "items",
+                  "key",
+                  "n",
+                  "timestamp",
+                  "type",
+                  "unnest",
+                  "."))
 
 #' Return column names missing from a data frame
 #' @param .data Data frame.
@@ -12,6 +26,6 @@ missing_col_names <- function(.data, ...) {
   UseMethod("missing_col_names")
 }
 missing_col_names.data.frame <- function(.data, ...) {
-  col_names <- map_chr(rlang::enquos(...), rlang::quo_text)
+  col_names <- purrr::map_chr(rlang::enquos(...), rlang::quo_text)
   col_names[!(col_names %in% colnames(.data))]
 }
