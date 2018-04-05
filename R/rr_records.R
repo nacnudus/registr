b97e2ec3b4dd23de10e3cb34fb91dc89167d0c6c
Duncan Garmonsway <nacnudus@gmail.com>
2018-04-05 15:15:46 +0100

    rr_records() to construct records from entries
    BREAKING CHANGE because previousl rr_records downloaded the tsv representation
    of a register.

--- R/rr_records.R
+++ R/rr_records.R
@@ -1,52 +1,91 @@
-#' @title Download All Register Records
-#' @description Download all records from a register, automatically handling
-#'              pages of up to 5000 records each and displaying a progress bar.
+#' A snapshot of records from given entries
 #'
-#'              A 'record' is the latest entry per key, so the record count is
-#'              the number of unique keys in a register.
+#' @description Filters for entries up to a given `entry-number` or `timestamp`,
+#'   then returns the latest entry per `key`.
 #'
-#'              The progress bar can be disabled by setting
-#'              `options("dplyr.show_progress" = FALSE)` and running the
-#'              function inside `suppressMessages()` (see the examples).
-#' @param register character, name of the register, e.g. "school-eng"
-#' @param phase character, one of "beta", "alpha", default: "beta"
-#' @param page_size numeric, number of records per page of download, default: 5000
-#' @return A tibble of all the records in a register
-#' @details Does *not* include all entries, only the latest entry per record.
+#'   Use [rr_snapshot()] for a snapshot of a whole register (both schema and
+#'   data).
+#'
+#' @param entries A data frame or tibble with at least the columns `key` and
+#' either `entry-number` or `timestamp`, depending on the value of `sequence`.
+#' @param sequence One of `"entry-number"` (default) or `"timestamp"`.  The
+#'   snapshot is taken at the `maximum` value of this column of `entries`.
+#' @param maximum An `integer` if `sequence` is `"entry-number" or `POSIXct` if
+#'   `sequence` is `"timestamp"`, giving the time at which to take the snapshot.
+#'   Only the latest entry up to this value will be kept, per `key`.  By default
+#'   it is the maximum of all entries, to return the most recent state of the
+#'   register.
+#' @param include_maximum Logical, whether to include entries whose
+#' `entry-number` or `timestamp` equals `maximum`.
+#'
+#' @return A tibble of records (latest entry per key).
+#'
+#' @details
+#' A record is the latest entry for a given key.  'Latest' can be either the
+#' maximum `entry-number` (recommended), or the maximum `timestamp`.  An entry
+#' `timestamp` may be `NA`, and may not be unique.  If the `timestamp` of any
+#' entry of a given key is `NA`, then no record for that key will be returned.
+#' If the `timestamp` is not unique, then the entry with the maximum
+#' `entry-number` will be chosen.
+#'
 #' @examples
 #' \dontrun{
 #' if(interactive()){
-#'  rr_records("country", "beta")
+#' country <- rr_register("country")
 #'
-#'  # To suppress the progress bar
-#'  options("dplyr.show_progress" = FALSE)
-#'  suppressMessages(rr_records("country", "beta"))
+#' country$data$entries %>%
+#'   filter(key == "CZ") %>%
+#'   select(`entry-number`, timestamp, name, `official-name`)
+#'
+#' country$data$entries %>%
+#'   rr_records(maximum = 64) %>%
+#'   filter(key == "CZ") %>%
+#'   select(`entry-number`, timestamp, name, `official-name`)
+#'
+#' country$data$entries %>%
+#'   rr_records(maximum = 217, include_maximum = FALSE) %>%
+#'   filter(key == "CZ") %>%
+#'   select(`entry-number`, timestamp, name, `official-name`)
+#'
+#' country$data$entries %>%
+#'   rr_records(sequence = "timestamp",
+#'              maximum = as.POSIXct("2016-04-05 13:23:05", tz = "UTC")) %>%
+#'   filter(key == "CZ") %>%
+#'   select(`entry-number`, timestamp, name, `official-name`)
+#'
+#' entries <- country$data$entries
+#' entries$timestamp[entries$`entry-number` == 64] <- NA
+#' rr_records(entries, sequence = "timestamp") %>%
+#'   filter(key == "CZ")
+#'
+#' # Not all entries are data, some are schema
+#' country$schema$custodians %>%
+#'   rr_records(maximum = 11) %>%
+#'   select(`entry-number`, timestamp, custodian)
 #'  }
 #' }
 #' @rdname rr_records
 #' @export
-rr_records <- function(register, phase = "beta", page_size = 5000) {
-  record_count <- rr_record_count(register, phase)
-  page_count <- ceiling(record_count / page_size)
-  base_url <-
-    dplyr::if_else(phase == "beta",
-            "https://{register}.register.gov.uk/records.tsv?page-size={page_size}&page-index={page_index}",
-            "https://{register}.{phase}.openregister.org/records.tsv?page-size={page_size}&page-index={page_index}")
-  p <- dplyr::progress_estimated(page_count)
-  message("Downloading register '", register, "' from the '", phase, "' phase ...\n")
-  out <-
-    purrr::map_df(seq_len(page_count),
-                  function(page_index) {
-                    out <-
-                      base_url %>%
-                      glue::glue() %>%
-                      readr::read_tsv(col_types = readr::cols(.default = "c")) # until all pages downloaded
-                    p$tick()$print()
-                    out
-                  }) %>%
-    dplyr::mutate_all(readr::parse_guess) %>%
-    dplyr::mutate(register = register, phase = phase) %>%
-    dplyr::select(register, phase, dplyr::everything())
-  message("\n\n")
-  out
+rr_records <- function(entries, sequence = c("entry-number", "timestamp"),
+                       maximum = NULL, include_maximum = TRUE) {
+  seq_string <- match.arg(sequence)
+  seq_sym <- rlang::sym(seq_string)
+  missing_columns <- missing_col_names(entries, key, !! seq_sym)
+  if (length(missing_columns) > 0L) {
+    stop("`entries` is missing the columns:",
+         paste0("\n    - `", missing_columns, "`"))
+  }
+  if (is.null(maximum)) {
+    maximum <- switch(seq_string,
+                      `entry-number` = max(entries$`entry-number`),
+                      timestamp = max(entries$`timestamp`))
+  }
+  if (include_maximum) `%</=%` <- `<=` else `%</=%` <- `<`
+  entries %>%
+    dplyr::filter(!! seq_sym %</=% maximum) %>%
+    dplyr::group_by(key) %>%
+    dplyr::arrange(desc(!! seq_sym), desc(`entry-number`)) %>%
+    dplyr::slice(1L) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(!! seq_sym)
 }
--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,14 @@ globalVariables(c("data",
                   "item_hash",
                   "item-hash"))
 
+#' Return column names missing from a data frame
+#' @param .data Data frame.
+#' @param ... names of columns.
+#' @return A character vector of column names that aren't in `.data`.
+missing_col_names <- function(.data, ...) {
+  UseMethod("missing_col_names")
+}
+missing_col_names.data.frame <- function(.data, ...) {
+  col_names <- map_chr(rlang::enquos(...), rlang::quo_text)
+  col_names[!(col_names %in% colnames(.data))]
+}
